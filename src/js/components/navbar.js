c991131ab59a636f4f3b63535b118d2ef8f5c6cc
AnassBouraada <anass.bouraada@cirvianum.cat>
2024-06-02 16:39:59 +0200

    afegir index, dinamicament i borrar el boto de clases

---
+++ src/js/components/navbar.js
@@ -0,0 +1,19 @@
+// header.js
+const header = `
+<header class="bg-gray-800 p-6">
+    <div class="container mx-auto flex flex-col md:flex-row items-center justify-between">
+        <div class="text-yellow-500 mb-4 md:mb-0 md:mr-4">
+            <h1 class="text-4xl font-serif">GOLD GYM A.B</h1>
+        </div>
+        <div class="flex items-center w-30 h-20 mr-4">
+            <div class="flex items-center">
+                <button id="login" class="bg-yellow-500 text-gray-800 py-2 px-4 rounded mr-2">Login</button>
+                <button id="logout" class="bg-red-500 text-white py-2 px-4 rounded hidden mx-4">Logout</button>
+                <button id="backButton" class="bg-gray-500 text-white py-2 px-4 rounded mx-4">Enrere</button>
+            </div>
+        </div>
+    </div>
+</header>
+`;
+
+export default header;
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,12 @@
 import { isAuthenticated, logout } from './auth.js';
+import header from '../js/components/navbar.js';
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    // Agrega el encabezado al inicio del body
+    document.body.insertAdjacentHTML('afterbegin', header);
+
+
     const loginButton = document.getElementById("login");
     const logoutButton = document.getElementById("logout");
     const classesButton = document.getElementById("classes");
@@ -22,4 +28,23 @@ document.addEventListener("DOMContentLoaded", () => {
         logout();
         window.location.href = "index.html";
     });
+
+    //comprovem si la ruta es index.htnml per mostrar el botó enrere
+    if (window.location.pathname === "/index.html") {
+        document.getElementById("backButton").classList.add("hidden");
+    }
+
+    // Event listener per el botó "Enrere"
+    const backButton = document.getElementById('backButton');
+    backButton.addEventListener('click', () => {
+        window.history.back();
+    });
+    
+    // Event listener per el botó "Login"
+    loginButton.addEventListener('click', () => {
+        window.location.href = 'quiSom.html';
+    });
+    
+    
+
 });
