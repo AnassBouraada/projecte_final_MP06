32391aaf0de42c1875650467b1040100329e322d
AnassBouraada <anass.bouraada@cirvianum.cat>
2024-06-03 19:46:54 +0200

    delete activites from user completed

--- src/js/classes.js
+++ src/js/classes.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
 });
 
-function mostrarClases(clases) {
+async function mostrarClases(clases) {
     const clasesListContainer = document.getElementById('clases-list');
     console.log('clasesListContainer', clasesListContainer);
     if (!clasesListContainer) {
@@ -24,48 +24,69 @@ function mostrarClases(clases) {
 
     clasesListContainer.innerHTML = '';
 
-    clases.forEach(clase => {
-        const claseRow = document.createElement('tr');
-        claseRow.innerHTML = `
-            <td class="border px-6 py-4">${clase.name}</td>
-            <td class="border px-6 py-4">${clase.duration}</td>
-            <td class="border px-6 py-4">${clase.type}</td>
-            <td class="border px-6 py-4">${clase.isIndividual ? 'Individual' : 'Grupal'}</td>
-            <td class="border px-6 py-4">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded transition duration-300" data-clase-id="${clase.id}">Registrar</button>
-            </td>
-        `;
-        claseRow.querySelector('button').addEventListener('click', async (event) => {
-            const claseId = parseInt(event.target.dataset.claseId, 10);
-            console.log('Registrar clase con ID:', claseId);
+    try {
+        const user = getLoggedInUser();
+        if (!isAuthenticated()) {
+            clases.forEach(clase => {
+                const claseRow = document.createElement('tr');
+                claseRow.innerHTML = `
+                    <td class="border px-6 py-4">${clase.name}</td>
+                    <td class="border px-6 py-4">${clase.duration}</td>
+                    <td class="border px-6 py-4">${clase.type}</td>
+                    <td class="border px-6 py-4">${clase.isIndividual ? 'Individual' : 'Grupal'}</td>
+                `;
+                clasesListContainer.appendChild(claseRow);
+            });
+            return;
+        }
 
-            if (!isAuthenticated()) {
-                window.location.href = 'quiSom.html';
-                return;
+        const userApi = await fetchFromApi(`users/${user.id}`);
+        clases.forEach(clase => {
+            const claseRow = document.createElement('tr');
+            claseRow.innerHTML = `
+                <td class="border px-6 py-4">${clase.name}</td>
+                <td class="border px-6 py-4">${clase.duration}</td>
+                <td class="border px-6 py-4">${clase.type}</td>
+                <td class="border px-6 py-4">${clase.isIndividual ? 'Individual' : 'Grupal'}</td>
+                <td class="border px-6 py-4">
+                    <button class="py-2 px-4 rounded transition duration-300" data-clase-id="${clase.id}">
+                        ${userApi.registration.includes(clase.id) ? 'Ya está inscrito' : 'Registrar'}
+                    </button>
+                </td>
+            `;
+
+            const registerButton = claseRow.querySelector('button');
+
+            if (userApi.registration.includes(clase.id)) {
+                registerButton.classList.add('bg-green-500', 'text-white', 'cursor-not-allowed');
+                registerButton.disabled = true;
+            } else {
+                registerButton.classList.add('bg-blue-500', 'hover:bg-blue-700', 'text-white');
+                registerButton.addEventListener('click', async (event) => {
+                    const claseId = parseInt(event.target.dataset.claseId, 10);
+                    console.log('Registrar clase con ID:', claseId);
+
+                    try {
+                        userApi.registration.push(claseId);
+                        console.log('Usuario actualizado con la clase registrada:', userApi);
+                        await fetch(`http://localhost:3001/users/${user.id}`, {
+                            method: "PUT",
+                            body: JSON.stringify(userApi), // Actualiza el usuario con el objeto completo
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                        });
+                        console.log(`Clase con ID ${claseId} registrada exitosamente para el usuario.`);
+                        window.location.href = 'userClasses.html';
+                    } catch (error) {
+                        console.error('Error al registrar la clase:', error);
+                    }
+                });
             }
 
-            try {
-                const userLocal = getLoggedInUser();
-                const user = await fetchFromApi(`users/${userLocal.id}`);
-                if (!user.registration.includes(claseId)) {
-                    user.registration.push(claseId);
-                    console.log('Usuario actualizado con la clase registrada:', user);
-                    await fetch(`http://localhost:3001/users/${user.id}`, {
-                        method: "PUT",
-                        body: JSON.stringify(user), // Actualiza el usuario con el objeto completo
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                    });
-                    console.log(`Clase con ID ${claseId} registrada exitosamente para el usuario.`);
-                    window.location.href = 'userClasses.html';
-                } else {
-                    console.log('El usuario ya está registrado en esta clase');
-                }
-            } catch (error) {
-                console.error('Error al registrar la clase:', error);
-            }
+            clasesListContainer.appendChild(claseRow);
         });
-        clasesListContainer.appendChild(claseRow);
-    });
+    } catch (error) {
+        console.error('Error al obtener el usuario:', error);
+    }
 }
--- src/js/userClasses.js
+++ src/js/userClasses.js
@@ -1,3 +1,5 @@
+// misClases.js
+
 import fetchFromApi from './fetchAPI.js';
 import { getLoggedInUser } from './auth.js';
 
@@ -33,7 +35,40 @@ function mostrarClases(clases) {
             <td class="border px-6 py-4">${clase.duration}</td>
             <td class="border px-6 py-4">${clase.type}</td>
             <td class="border px-6 py-4">${clase.isIndividual ? 'Individual' : 'Grupal'}</td>
+            <td class="border px-6 py-4">
+                <button class="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded transition duration-300" data-clase-id="${clase.id}">Desapuntarse</button>
+            </td>
         `;
+
+        const unregisterButton = claseRow.querySelector('button');
+
+        unregisterButton.addEventListener('click', async (event) => {
+            const claseId = parseInt(event.target.dataset.claseId, 10);
+            console.log('Desapuntarse de la clase con ID:', claseId);
+
+            try {
+                const userLocal = getLoggedInUser();
+                console.log('Usuario local:', userLocal);
+                const response = await fetch(`http://localhost:3001/users/${userLocal.id}`);
+                const userData = await response.json(); // Extraer los datos del cuerpo de la respuesta
+                console.log('Datos del usuario obtenidos:', userData);
+                const updatedRegistrations = userData.registration.filter(id => id !== claseId);
+                console.log('updatedRegistrations:', updatedRegistrations);
+                userData.registration = updatedRegistrations;
+                await fetch(`http://localhost:3001/users/${userLocal.id}`, {
+                    method: "PUT",
+                    body: JSON.stringify(userData),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+                console.log(`Usuario desapuntado de la clase con ID ${claseId}.`);
+                window.location.reload(); // Recargar la página para mostrar los cambios
+            } catch (error) {
+                console.error('Error al desapuntarse de la clase:', error);
+            }
+        });
+
         clasesListContainer.appendChild(claseRow);
     });
 }
